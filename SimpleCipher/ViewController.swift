26ba322a72ab7c919e6be240c32562a66b97d8b2
U3 <U3@u3s-macbook-pro.local>
2015-09-27 13:58:48 -0400

    Initial Commit

---
+++ SimpleCipher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCipher
+//
+//  Created by U3 on 2015-09-27.
+//  Copyright © 2015 Ten Kettles Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SimpleCipherTests/SimpleCipherTests.swift
@@ -0,0 +1,36 @@
+//
+//  SimpleCipherTests.swift
+//  SimpleCipherTests
+//
+//  Created by U3 on 2015-09-27.
+//  Copyright © 2015 Ten Kettles Inc. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleCipher
+
+class SimpleCipherTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
